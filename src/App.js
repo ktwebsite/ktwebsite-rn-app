10e427b1926a480286b4814fc71e17135962da9f
ktwebsite <dangchankhang@gmail.com>
2022-07-06 17:08:59 +0700

    KTWebsite App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import { Button, StyleSheet, Text, View, Alert, Platform } from 'react-native';
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>click.js</Text>
-    </View>
-  );
-}
+  const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      backgroundColor: '#fff',
+      padding: "15px",
+      fontFamily: "WhitneyMedium"
+    },
+    header: {
+      fontSize: 30,
+      fontFamily: "WhitneyBold",
+      marginTop: "10px",
+      marginBottom: "10px",
+    },
+  });
+  // const [fontsLoaded, setFontsLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    backgroundColor: '#fff',
-    padding: "15px",
-    fontFamily: "whitneymedium"
-  },
-  header: {
-    fontSize: 30,
-    fontFamily: "whitneysemibold",
-    marginTop: "5px",
-    marginBottom: "5px",
-  },
-});
+  const [fontsLoaded] = useFonts({
+    WhitneyMedium: require('../assets/fonts/whitneymedium.otf'),
+    WhitneyBold: require('../assets/fonts/whitneybold.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome to KTWebsite, btw i use arch</Text>
+      </View>
+    );
+  }
+}
